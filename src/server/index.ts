6ea1fb5f33fbd79ad39e0cbf3113ef8996e4f489
Ward <wardpearce@protonmail.com>
2021-05-08 17:25:43 +1200

    Added Server.get

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import HTTP from '../http'
 
+import { IServer } from '../interfaces/server'
+
 export default class Server {
     #http
     serverId: string
@@ -7,5 +9,9 @@ export default class Server {
     constructor (serverId: string, http: HTTP) {
         this.#http = http
         this.serverId = serverId
+    }
+
+    public async get(): Promise<IServer> {
+        return <IServer>await this.#http.get(`/game-servers/${this.serverId}`)
     }
 }
