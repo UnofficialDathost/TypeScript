8827c92709a1301bfd0c34b2806d815c9675a4e7
Ward <wardpearce@protonmail.com>
2021-05-10 11:23:12 +1200

    Added file actions

--- src/server/file.ts
+++ src/server/file.ts
@@ -1,4 +1,5 @@
 import HTTP from '../http'
+import Server from './'
 
 export default class File {
   serverId: string
@@ -9,5 +10,34 @@ export default class File {
     this.serverId = serverId
     this.path = path
     this.#http = http
+  }
+
+  public get url(): string {
+    return `/game-servers/${this.serverId}/files/${this.path}`
+  }
+
+  public async unzip(destination: string = null): Promise<void> {
+    let payload: URLSearchParams
+    if (destination != null) {
+      payload = new URLSearchParams()
+      payload.append('destination', destination)
+    } else
+      payload = null
+
+    await this.#http.post(`/game-servers/${this.serverId}/unzip/${this.path}`, payload)
+  }
+
+  public async delete(): Promise<void> {
+    await this.#http.delete(this.url)
+  }
+
+  public async move(destination: string): Promise<void> {
+    const payload = new URLSearchParams()
+    payload.append('destination', destination)
+    await this.#http.put(this.url, payload)
+  }
+
+  public async download(asText = false): Promise<ArrayBuffer> {
+    return await this.#http.get(`${this.url}?as_text=${asText.toString()}`)
   }
 }
